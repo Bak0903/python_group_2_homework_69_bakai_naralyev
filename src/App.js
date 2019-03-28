8c0235d7a3dfe5429fb3598f6e455e5258acc624
bak0903 <bak0903@mail.ru>
2019-03-28 15:23:45 +0600

    Добавил контейнер Calc для калькулятора, добавил его в App. Написал стили.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Calc from './containers/Calc/Calc';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    render() {
+        return (
+        <div className="App">
+            <Calc/>
+        </div>
+        );
+    }
 }
 
 export default App;
